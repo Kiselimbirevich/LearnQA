a21c35e0ea4018bfbd19bfbd75c510395f70cad2
Ernest <glowingacid2@gmail.com>
2025-08-23 00:17:49 +0300

    bob

--- lib/my_requests.py
+++ lib/my_requests.py
@@ -1,6 +1,7 @@
 import requests.exceptions
 from logs.logger import Logger
 import allure
+from enviroment import ENV_OBJECT
 
 class MyRequests():
     @staticmethod
@@ -28,7 +29,7 @@ class MyRequests():
     @staticmethod
     def _send(url: str, data: dict, headers: dict, cookies: dict, method: str):
 
-        url = f"https://playground.learnqa.ru/api{url}"
+        url = f"{ENV_OBJECT.get_base_url()}{url}"
 
         if headers is None:
             headers = {}
